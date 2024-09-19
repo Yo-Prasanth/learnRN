919923dc034ddd27e6a638b0288c51ca332e5edd
Yo-Prasanth <yo.prasanth04@gmail.com>
2024-09-19 10:42:12 +0530

    add TouchableOpacity

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,11 +1,15 @@
 // HomeScreen.js
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, TouchableOpacity, GestureResponderEvent } from 'react-native';
 import IntroCard from '../components/IntroCard';
 import TweetPage from './TweetPage';
 
 
 const HomePage = () => {
+
+    function onPress(event: GestureResponderEvent): void {
+        throw new Error('Function not implemented.');
+    }
 
     return <View style={{ flex: 1, backgroundColor: "white",padding:5 }}>
         <IntroCard/>
@@ -16,14 +20,15 @@ const HomePage = () => {
                     <View>
                     <Text style={{color:"black", fontSize:22}}>Follow me on </Text>
                     </View>
-                    <View style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></View>
-                    <View style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></View>
-                    <View style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></View>
+                    <TouchableOpacity style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></TouchableOpacity>
+                    <TouchableOpacity style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></TouchableOpacity>
+                    <TouchableOpacity style={{height:30, width:30, backgroundColor:"#d3d3d3", borderRadius:5}}></TouchableOpacity>
+
                 </View>
                 <View style={{flex:5, backgroundColor:"white", padding:5}}>
                     <Text style={{color:"black", fontSize:20}}>Recent Projects & Movies</Text>
                     <View style={{height:"70%",justifyContent:"space-around"}}>
-                    <View style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
+                    <TouchableOpacity style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
                         <View style={{alignItems:"center", justifyContent:"center", paddingStart:10}}>
                             <View style={{height:30, width:30, backgroundColor:"white", borderRadius:5}}>
                             </View>
@@ -32,8 +37,8 @@ const HomePage = () => {
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>Kannappa</Text>
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>2024</Text>
                         </View>
-                    </View>
-                    <View style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
                         <View style={{alignItems:"center", justifyContent:"center", paddingStart:10}}>
                             <View style={{height:30, width:30, backgroundColor:"white", borderRadius:5}}>
                             </View>
@@ -42,7 +47,8 @@ const HomePage = () => {
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>Star</Text>
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>2024</Text>
                         </View>
-                    </View><View style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
                         <View style={{alignItems:"center", justifyContent:"center", paddingStart:10}}>
                             <View style={{height:30, width:30, backgroundColor:"white", borderRadius:5}}>
                             </View>
@@ -51,7 +57,8 @@ const HomePage = () => {
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>Om Bheem Bush</Text>
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>2024</Text>
                         </View>
-                    </View><View style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{backgroundColor:"#d3d3d3", borderRadius:5, flexDirection:"row",height:"20%"}}>
                         <View style={{alignItems:"center", justifyContent:"center", paddingStart:10}}>
                             <View style={{height:30, width:30, backgroundColor:"white", borderRadius:5}}>
                             </View>
@@ -60,7 +67,7 @@ const HomePage = () => {
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>Maine Pyar Kiya</Text>
                             <Text style={{color:"black", fontSize:15, paddingStart:20}}>2024</Text>
                         </View>
-                    </View>
+                    </TouchableOpacity>
                     </View>
 
                 </View>
